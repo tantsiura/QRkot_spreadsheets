5da28aa86bb0e7b223127d13df9c2fc679d2aadf
tantsiura <97056496+tantsiura@users.noreply.github.com>
2023-12-28 12:01:01 +0300

    fix tests

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 
 from app.api.routers import main_router
-#from app.core.config import settings
+from app.core.config import settings
 from app.core.init_db import create_first_superuser
 
-#app = FastAPI(
-#    title=settings.app_title,
-#    description=settings.app_desc
-#)
-
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_title,
+    description=settings.app_desc
+)
 
 app.include_router(main_router)
 
